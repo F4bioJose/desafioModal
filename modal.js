3c888cd757fad4b13b54502d80a43674f7a604e9
Fábio José <fabio02d2@gmail.com>
2023-07-23 19:35:10 -0300

    feat: finished js

--- modal.js
+++ modal.js
@@ -1,1 +1,29 @@
-const openModal = document.getElementById('openModal')
+const openModal = document.getElementById("openModal")
+const body = document.querySelector('body')
+const modalWrapper = document.querySelector('.modalWrapper')
+
+
+ function clickOnButton() {
+     openModal.style.display = "none";
+     body.style.backgroundColor = "#7f828c";
+     modalWrapper.style.display = "block";
+ }
+
+ openModal.addEventListener('click',clickOnButton)
+
+ document.addEventListener('keydown', clickOnEscButton)
+
+ function clickOnEscButton(event) {
+    
+    if(event.key === 'Escape') {
+        openModal.style.display = "block";
+        body.style.backgroundColor = "#cec2e6";
+        modalWrapper.style.display = "none";
+        }
+    }
+
+
+
+
+
+
